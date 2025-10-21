c9f44db9311baad96c38d76cf7ece6e7057df209
RabagoAI <pacoalfair@hotmail.com>
2025-10-21 21:44:21 +0200

    fix: TypeScript error in mongodb connection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -3,8 +3,12 @@ import { MongoClient } from 'mongodb'
 const uri = process.env.MONGODB_URI!
 const options = {}
 
-let client
+let client: MongoClient
 let clientPromise: Promise<MongoClient>
+
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined
+}
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local')
